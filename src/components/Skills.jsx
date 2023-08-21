331189faae388241d85671008873a5532ffc4728
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-08-21 23:00:09 +0600

    Skill section updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,57 +1,69 @@
-import React from "react";
+// import React from "react";
+
+// const Skills = () => {
+//   const skills = [
+//     {
+//       logo: "logo-html5",
+//       level: "Advance",
+//       count: 86,
+//     },
+//     {
+//       logo: "logo-css3",
+//       level: "Expect",
+//       count: 90,
+//     },
+//     {
+//       logo: "logo-nodejs",
+//       level: "Beginner",
+//       count: 40,
+//     },
+//     {
+//       logo: "logo-react",
+//       level: "Intermediate",
+//       count: 80,
+//     },
+//   ];
+//   return (
+//     <section id="skills" className="relative py-10 bg-gray-800">
+//       <div className="mt-8 text-center text-gray-100">
+//         <h3 className="text-4xl font-semibold">
+//           My <span className="text-purple-600">Skills</span>
+//         </h3>
+//         <p className="mt-3 text-lg text-gray-400">My knowledge</p>
+//         <div className="flex flex-wrap items-center justify-center gap-10 mt-12">
+//           {skills?.map((skill, i) => (
+//             <div
+//               key={i}
+//               className="border-2 group border-purple-600 relative min-w-[10rem] max-w-[16rem] bg-gray-900 p-10 rounded-xl"
+//             >
+//               <div
+//                 style={{
+//                   background: `conic-gradient(rgb(8,145,170) ${skill.count}%,#ddd ${skill.count}%)`,
+//                 }}
+//                 className="flex items-center justify-center w-32 h-32 rounded-full"
+//               >
+//                 <div className="flex items-center justify-center text-6xl bg-gray-900 rounded-full w-28 h-28 group-hover:text-purple-600">
+//                   <ion-icon name={skill.logo}></ion-icon>
+//                 </div>
+//               </div>
+//               <p className="mt-3 text-xl">{skill.level}</p>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Skills;
+
+import React from 'react';
 
 const Skills = () => {
-  const skills = [
-    {
-      logo: "logo-html5",
-      level: "Advance",
-      count: 86,
-    },
-    {
-      logo: "logo-css3",
-      level: "Expect",
-      count: 90,
-    },
-    {
-      logo: "logo-nodejs",
-      level: "Beginner",
-      count: 40,
-    },
-    {
-      logo: "logo-react",
-      level: "Intermediate",
-      count: 80,
-    },
-  ];
   return (
-    <section id="skills" className="py-10 bg-gray-800 relative">
-      <div className="mt-8 text-gray-100 text-center">
-        <h3 className="text-4xl font-semibold">
-          My <span className="text-purple-600">Skills</span>
-        </h3>
-        <p className="text-gray-400 mt-3 text-lg">My knowledge</p>
-        <div className="flex items-center justify-center mt-12 gap-10 flex-wrap">
-          {skills?.map((skill, i) => (
-            <div
-              key={i}
-              className="border-2 group border-purple-600 relative min-w-[10rem] max-w-[16rem] bg-gray-900 p-10 rounded-xl"
-            >
-              <div
-                style={{
-                  background: `conic-gradient(rgb(8,145,170) ${skill.count}%,#ddd ${skill.count}%)`,
-                }}
-                className="w-32 h-32 flex items-center justify-center rounded-full"
-              >
-                <div className="text-6xl w-28 h-28 bg-gray-900 rounded-full flex items-center justify-center group-hover:text-purple-600">
-                  <ion-icon name={skill.logo}></ion-icon>
-                </div>
-              </div>
-              <p className="text-xl mt-3">{skill.level}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
+    <div>
+      <p>Skill</p>
+    </div>
   );
 };
 
