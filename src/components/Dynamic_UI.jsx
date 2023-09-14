f9450cdfb52a04716e07c14fb52161ea2d905e37
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-09-14 23:45:33 +0600

    Dynamic UI

---
+++ src/components/Dynamic_UI.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dynamic_UI = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium, vel.
+            </p>
+        </div>
+    );
+};
+
+export default Dynamic_UI;
