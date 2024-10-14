ba051bad83b15a1c605b03880807523d386188cd
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2024-10-14 22:22:10 +0600

    Navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,23 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 import { FiMenu } from "react-icons/fi";
 import { MdClose } from "react-icons/md";
 import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
-import {logo} from "../../assets/index"
-import { navLinksdata } from '../../constants';
+import { logo } from "../../assets/index";
+import { navLinksdata } from "../../constants";
 
 const Navbar = () => {
-  const [showMenu, setShowMenu]=useState(false)
+  const [showMenu, setShowMenu] = useState(false);
+  const [activeLink, setActiveLink] = useState("home");
+
   return (
     <div className="w-full h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
       <div>
-        <img src={logo} alt="logo" />
+        <img className="w-1/4 rounded-full" src={logo} alt="logo" />
       </div>
       <div>
-        <ul className="hidden mdl:inline-flex items-center gap-6 lg:gap-10">
+        <ul className="items-center hidden gap-6 mdl:inline-flex lg:gap-10">
           {navLinksdata.map(({ _id, title, link }) => (
             <li
-              className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300"
+              className={`text-base font-normal tracking-wide duration-300 cursor-pointer xl:p-1 ${
+                activeLink === link
+                  ? "text-designColor"
+                  : "text-slate-100 hover:text-cyan-300"
+              }`}
               key={_id}
             >
               <Link
@@ -25,8 +31,9 @@ const Navbar = () => {
                 to={link}
                 spy={true}
                 smooth={true}
-                offset={-70}
+                offset={-100} // Adjust the offset if needed
                 duration={500}
+                onSetActive={() => setActiveLink(link)}
               >
                 {title}
               </Link>
@@ -35,16 +42,16 @@ const Navbar = () => {
         </ul>
         <span
           onClick={() => setShowMenu(!showMenu)}
-          className="text-xl mdl:hidden bg-black w-10 h-10 inline-flex items-center justify-center rounded-full text-designColor cursor-pointer"
+          className="inline-flex items-center justify-center w-10 h-10 text-xl bg-black rounded-full cursor-pointer mdl:hidden text-designColor"
         >
           <FiMenu />
         </span>
         {showMenu && (
           <div className="w-[80%] h-screen overflow-scroll absolute top-0 left-0 bg-gray-900 p-4 scrollbar-hide">
-            <div className="flex flex-col gap-8 py-2 relative">
+            <div className="relative flex flex-col gap-8 py-2">
               <div>
                 <img className="w-32" src={logo} alt="logo" />
-                <p className="text-sm text-gray-400 mt-2">
+                <p className="mt-2 text-sm text-gray-400">
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                   Earum soluta perspiciatis molestias enim cum repellat, magnam
                   exercitationem distinctio aliquid nam.
@@ -54,7 +61,7 @@ const Navbar = () => {
                 {navLinksdata.map((item) => (
                   <li
                     key={item._id}
-                    className="text-base font-normal text-gray-400 tracking-wide cursor-pointer hover:text-designColor duration-300"
+                    className="text-base font-normal tracking-wide text-gray-400 duration-300 cursor-pointer hover:text-designColor"
                   >
                     <Link
                       onClick={() => setShowMenu(false)}
@@ -62,8 +69,9 @@ const Navbar = () => {
                       to={item.link}
                       spy={true}
                       smooth={true}
-                      offset={-70}
+                      offset={-100}
                       duration={500}
+                      onSetActive={() => setActiveLink(item.link)}
                     >
                       {item.title}
                     </Link>
@@ -71,7 +79,7 @@ const Navbar = () => {
                 ))}
               </ul>
               <div className="flex flex-col gap-4">
-                <h2 className="text-base uppercase font-titleFont mb-4">
+                <h2 className="mb-4 text-base uppercase font-titleFont">
                   Find me in
                 </h2>
                 <div className="flex gap-4">
@@ -88,7 +96,7 @@ const Navbar = () => {
               </div>
               <span
                 onClick={() => setShowMenu(false)}
-                className="absolute top-4 right-4 text-gray-400 hover:text-designColor duration-300 text-2xl cursor-pointer"
+                className="absolute text-2xl text-gray-400 duration-300 cursor-pointer top-4 right-4 hover:text-designColor"
               >
                 <MdClose />
               </span>
@@ -98,6 +106,6 @@ const Navbar = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
