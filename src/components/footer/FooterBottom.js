36b7c5b0198c80244b36b7fe97dbb9d673b59ba9
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2024-10-14 22:22:28 +0600

    Footer

--- src/components/footer/FooterBottom.js
+++ src/components/footer/FooterBottom.js
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const FooterBottom = () => {
   return (
     <div className="w-full py-10">
-      <p className="text-center text-gray-500 text-base">
-        © 2022. All rights reserved by Noor Mohammad
+      <p className="text-base text-center text-gray-500">
+        © All rights reserved by Mujahidul Islam
       </p>
     </div>
   );
-}
+};
 
-export default FooterBottom
+export default FooterBottom;
