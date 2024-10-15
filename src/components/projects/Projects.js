914ad8b49cae81d136da80a1b820398a70703ebe
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2024-10-15 21:56:16 +0600

    Projects Details

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import Title from '../layouts/Title'
+import React from "react";
+import Title from "../layouts/Title";
 import { projectOne, projectTwo, projectThree } from "../../assets/index";
-import ProjectsCard from './ProjectsCard';
+import ProjectsCard from "./ProjectsCard";
 
 const Projects = () => {
   return (
@@ -9,52 +9,69 @@ const Projects = () => {
       id="projects"
       className="w-full py-20 border-b-[1px] border-b-black"
     >
-      <div className="flex justify-center items-center text-center">
-        <Title
-          title="VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK"
-          des="My Projects"
-        />
+      <div className="flex items-center justify-center text-center">
+        <Title des="Projects" />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3 xl:gap-14">
         <ProjectsCard
-          title="SOCIAL MEDIA CLONE"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          title="Share Parking"
+          des=" A platform for users to easily share and rent parking spaces, making urban parking simpler and more accessible."
           src={projectOne}
+          link="https://www.google.com/"
+          liveLink="https://parking-sharing-89ed9.web.app/"
+          clientLink="https://github.com/Mujahidul12628/Parking-Share"
+          serverLink="https://github.com/Mujahidul12628/Parking-Sharing-Server"
+          techStack={[
+            "React JS",
+            "Node JS",
+            "Express",
+            "MongoDB",
+            "SSL Commerce",
+            "Firebase",
+            "Vercel",
+            "Nodemon",
+          ]}
         />
         <ProjectsCard
-          title="E-commerce Website"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          title="Virtual Academy "
+          des=" An interactive online platform where children can learn about various sports, enhancing their skills and knowledge."
           src={projectTwo}
+          link="https://www.youtube.com/"
+          liveLink="https://virtual-academy-93114.web.app/"
+          clientLink="https://github.com/Mujahidul12628/Virtual-Academy"
+          serverLink="https://github.com/Mujahidul12628/Virtual-Academy-Server"
+          techStack={[
+            "React JS",
+            "Tailwind CSS",
+            "React Spring",
+            "React Tab",
+            "Node JS",
+            "Express",
+            "Firebase",
+            "Vercel",
+          ]}
         />
         <ProjectsCard
-          title="Chatting App"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          title="Trek Vibes"
+          des=" A travel website that provides users with curated trekking experiences, guiding them to explore stunning landscapes."
           src={projectThree}
-        />
-        <ProjectsCard
-          title="SOCIAL MEDIA CLONE"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectThree}
-        />
-        <ProjectsCard
-          title="E-commerce Website"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectOne}
-        />
-        <ProjectsCard
-          title="Chatting App"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectTwo}
+          liveLink="https://trek-vibes.web.app/"
+          clientLink="https://github.com/Mujahidul12628/Travel"
+          serverLink="https://github.com/Mujahidul12628/Travel-Server"
+          techStack={[
+            "React JS",
+            "Tailwind",
+            "Countup",
+            "Swiper JS",
+            "Node JS",
+            "Express",
+            "Firebase",
+            "Vercel",
+          ]}
         />
       </div>
     </section>
   );
-}
+};
 
-export default Projects
+export default Projects;
--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -1,39 +1,112 @@
-import React from 'react'
+import React from "react";
 import { BsGithub } from "react-icons/bs";
 import { FaGlobe } from "react-icons/fa";
 
-const ProjectsCard = ({ title, des, src }) => {
+const ProjectsCard = ({
+  title,
+  des,
+  src,
+  liveLink,
+  clientLink,
+  serverLink,
+  techStack,
+}) => {
   return (
-    <div className="w-full p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000">
-      <div className="w-full h-[80%] overflow-hidden rounded-lg">
+    <div
+      className=" w-full p-4   h-auto  rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-[#433f3f] to-[#013c3f] group hover:bg-gradient-to-b hover:from-[#00a1be] hover:gray-900npm run dev
+    n transition-colors duration-1000 hover:gap-5"
+    >
+      <div className="w-full rounded-lg hover:border-spacing-4">
         <img
-          className="w-full h-60 object-cover group-hover:scale-110 duration-300 cursor-pointer"
+          className="w-full duration-300 cursor-pointer h-60 group-hover:scale-105"
           src={src}
           alt="src"
         />
       </div>
-      <div className="w-full mt-5 flex flex-col  gap-6">
+      <div className="flex flex-col w-full gap-5 mt-5 duration-300 cursor-pointer group-hover:scale-105">
         <div>
-          <div className="flex items-center justify-between">
-            <h3 className="text-base uppercase text-designColor font-normal">
+          <div className="">
+            <h3 className="p-1 text-base font-semibold uppercase text-cyan-400">
               {title}
             </h3>
-            <div className="flex gap-2">
-              <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
-                <BsGithub />
-              </span>
-              <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
-                <FaGlobe />
-              </span>
-            </div>
           </div>
-          <p className="text-sm tracking-wide mt-3 hover:text-gray-100 duration-300">
+          <p className="mt-3 text-sm tracking-wide duration-300 hover:text-gray-100">
             {des}
           </p>
+          {/* <div className="flex flex-wrap items-center py-2 text-sm">
+            <span> Technology:</span>
+            <span className="px-2 py-1 mx-2 my-1 text-xs rounded-sm tech-box">
+              React JS
+            </span>
+            <span className="px-2 py-1 mx-2 my-1 text-xs rounded-sm tech-box">
+              Tailwind
+            </span>
+            <span className="px-2 py-1 mx-2 my-1 text-xs rounded-sm tech-box">
+              Countup
+            </span>
+            <span className="px-2 py-1 mx-2 my-1 text-xs rounded-sm tech-box">
+              Swiper JS
+            </span>
+            <span className="px-2 py-1 mx-2 my-1 text-xs rounded-sm tech-box">
+              Node JS
+            </span>
+            <span className="px-2 py-1 mx-2 my-1 text-xs rounded-sm tech-box">
+              Express
+            </span>
+            <span className="px-2 py-1 mx-2 my-1 text-xs rounded-sm tech-box">
+              Firebase
+            </span>
+            <span className="px-2 py-1 mx-2 my-1 text-xs rounded-sm tech-box">
+              Vercel
+            </span>
+          </div> */}
+          <div className="flex flex-wrap items-center py-2 ">
+            <span> Technology:</span>
+            {techStack.map((tech, index) => (
+              <span
+                key={index}
+                className="px-2 py-1 mx-2 my-1 text-xs rounded-sm tech-box"
+              >
+                {tech}
+              </span>
+            ))}
+          </div>
+          <div className="flex gap-3 pt-3 pb-5 ">
+            <a
+              href={liveLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center justify-between gap-2 px-3 py-1 text-sm tracking-wide rounded-sm sm:text-xs md:text-sm lg:text-md hover:border-2 hover:border-cyan-500 bg-cyan-600 hover:bg-cyan-9 00"
+            >
+              <span className="p-1 text-base text-white duration-300 cursor-pointer hover:text-designColor">
+                <FaGlobe />
+              </span>
+              <span className="font-semibold">Live</span>
+            </a>
+            <a
+              href={clientLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="px-3 py-1 text-xs border rounded-sm xs:text-sm sm:text-xs md:text-sm lg:text-md border-cyan-500 hover:bg-cyan-600"
+            >
+              <span>Github-Client</span>
+            </a>
+            <a
+              href={serverLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="px-3 py-1 text-xs border rounded-sm xs:text-sm sm:text-xs md:text-sm lg:text-md border-cyan-500 hover:bg-cyan-600"
+            >
+              {/* <span className="inline-flex items-center justify-center w-10 h-10 text-lg text-gray-400 duration-300 bg-black rounded-full cursor-pointer hover:text-designColor">
+                    <BsGithub />
+                  </span> */}
+              <span>Github-Server</span>
+            </a>
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default ProjectsCard
+export default ProjectsCard;
